cdef3a02c13c4d5636e48dd5529651a0ce1552fd
Fokklz <chat@fokklz.dev>
2024-11-12 20:04:58 +0100

    feat(md-annotations): improved support
    Besides the #:: syntax, now it also supports #::[0-9] syntax to define a annotation number.
    This annotation number can then be used to write a multi-line text for it a the end of the code block.
    
    Example:
    ```javascript
    let some = code #::1
    
    ```
    
    This will render the annotation text at the end of the line with the content: `This is a annotation for the code above`.

--- assets/custom-techy/js/markdown-annotations.js
+++ assets/custom-techy/js/markdown-annotations.js
@@ -1,4 +1,4 @@
-// Dependencies: tippy.js, clipboard.js
+// Dependencies: tippy.js
 
 // Main function to process code elements
 function processCodeElements() {
@@ -15,41 +15,92 @@ function findAndMarkAnnotationsInCode(preElement, index) {
   const codeElement = preElement.querySelector("code");
   if (!codeElement) return;
 
-  const { fullText, nodeInfoList } = getFullTextAndNodeInfo(codeElement);
+  // Get full text and node info
+  let { fullText, nodeInfoList } = getFullTextAndNodeInfo(codeElement);
 
-  const regex = /#::.*$/gm;
-  const matches = [];
-  let match;
-  while ((match = regex.exec(fullText)) !== null) {
-    matches.push({
-      index: match.index,
-      length: match[0].length,
-      text: match[0],
+  // First, find and collect bottom annotations (Option 2)
+  const bottomAnnotationsRegex = /#(\d+)\?\?(.*?)(?=(\n#\d+\?\?|$))/gs;
+  const bottomAnnotations = {};
+  const bottomAnnotationMatches = [];
+  let bottomMatch;
+  while ((bottomMatch = bottomAnnotationsRegex.exec(fullText)) !== null) {
+    const number = bottomMatch[1];
+    const text = bottomMatch[2].trim();
+    bottomAnnotations[number] = text;
+
+    bottomAnnotationMatches.push({
+      index: bottomMatch.index,
+      length: bottomMatch[0].length,
     });
   }
 
-  let innerIndex = 0;
-
-  // Process matches in reverse order
-  for (let i = matches.length - 1; i >= 0; i--) {
-    const matchIndex = matches[i].index;
-    const matchLength = matches[i].length;
-    const matchText = matches[i].text;
-
+  // Remove bottom annotations from code, processing in reverse order
+  for (let i = bottomAnnotationMatches.length - 1; i >= 0; i--) {
+    const { index: matchIndex, length: matchLength } =
+      bottomAnnotationMatches[i];
     const startInfo = getNodeAndOffsetFromIndex(nodeInfoList, matchIndex);
     const endInfo = getNodeAndOffsetFromIndex(
       nodeInfoList,
       matchIndex + matchLength
     );
 
-    // Extract text after '#::'
-    const annotationText = matchText.substring(3).trim();
+    // Remove from DOM
+    const range = document.createRange();
+    range.setStart(startInfo.node, startInfo.offset);
+    range.setEnd(endInfo.node, endInfo.offset);
+    range.deleteContents();
+  }
+
+  // Recompute fullText and nodeInfoList after removal
+  const { fullText: updatedFullText, nodeInfoList: updatedNodeInfoList } =
+    getFullTextAndNodeInfo(codeElement);
+
+  // Find code annotations
+  const codeAnnotationsRegex = /#::(\d+)?(.*)$/gm;
+  const matches = [];
+  let match;
+  while ((match = codeAnnotationsRegex.exec(updatedFullText)) !== null) {
+    matches.push({
+      index: match.index,
+      length: match[0].length,
+      number: match[1],
+      text: match[2].trim(),
+    });
+  }
+
+  // Process matches in reverse order
+  for (let i = matches.length - 1; i >= 0; i--) {
+    const { index: matchIndex, length: matchLength, number, text } = matches[i];
+
+    const startInfo = getNodeAndOffsetFromIndex(
+      updatedNodeInfoList,
+      matchIndex
+    );
+    const endInfo = getNodeAndOffsetFromIndex(
+      updatedNodeInfoList,
+      matchIndex + matchLength
+    );
+
+    let annotationText;
+    if (number) {
+      // Option 2
+      annotationText = bottomAnnotations[number];
+    } else {
+      // Option 1
+      annotationText = text;
+    }
+
+    if (!annotationText) {
+      // No annotation text found; skip
+      continue;
+    }
 
     // Create a range covering the match
     const range = document.createRange();
     range.setStart(startInfo.node, startInfo.offset);
     range.setEnd(endInfo.node, endInfo.offset);
 
+    // Get bounding rect
     const rect = range.getBoundingClientRect();
     const codeRect = preElement.getBoundingClientRect();
 
@@ -57,11 +108,14 @@ function findAndMarkAnnotationsInCode(preElement, index) {
     const left = rect.left - codeRect.left + preElement.scrollLeft;
 
     const marker = document.createElement("a");
-    marker.id = `annotation-marker-${index}-${innerIndex++}`;
+    marker.id = `annotation-marker-${index}-${i}`;
     marker.className = "annotation-marker";
     marker.style.top = `${top - 2}px`;
     marker.style.left = `${left}px`;
-    marker.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="19px" height="19px"><path fill="#2196f3" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"/><path fill="#fff" d="M22 22h4v11h-4V22zM26.5 16.5c0 1.379-1.121 2.5-2.5 2.5s-2.5-1.121-2.5-2.5S22.621 14 24 14 26.5 15.121 26.5 16.5z"/></svg>`;
+    marker.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="19px" height="19px">
+      <path fill="#2196f3" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"/>
+      <path fill="#fff" d="M22 22h4v11h-4V22zM26.5 16.5c0 1.379-1.121 2.5-2.5 2.5s-2.5-1.121-2.5-2.5S22.621 14 24 14 26.5 15.121 26.5 16.5z"/>
+    </svg>`;
 
     preElement.appendChild(marker);
 
@@ -77,9 +131,11 @@ function findAndMarkAnnotationsInCode(preElement, index) {
       zIndex: 99999,
     });
 
+    // Remove the annotation from the code
     range.deleteContents();
   }
 }
+
 // Function to format markdown to HTML
 function formatMarkdownToHTML(markdownText) {
   // special case for line breaks
@@ -141,7 +197,7 @@ function getFullTextAndNodeInfo(element) {
 function getNodeAndOffsetFromIndex(nodeInfoList, index) {
   let currentIndex = 0;
   for (const info of nodeInfoList) {
-    if (currentIndex + info.length > index) {
+    if (currentIndex + info.length >= index) {
       return { node: info.node, offset: index - currentIndex };
     }
     currentIndex += info.length;
