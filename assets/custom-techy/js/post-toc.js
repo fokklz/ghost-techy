ca4b6a8ac8bc5d8daf5ffd8355fcb77cd1ccec7e
Fokklz <chat@fokklz.dev>
2024-11-15 09:05:56 +0100

    feat(post-toc): better mobile support
    Improved mobile experience by adding a button which allows to toggle the visibility of the table of contents anywhere in the post.
    Initial version, requires further testing and improvements i think.

--- assets/custom-techy/js/post-toc.js
+++ assets/custom-techy/js/post-toc.js
@@ -1,10 +1,34 @@
+var navHeight = 0;
+var postHeader = 0;
+
+// original version taken from: https://ghostfam.com/en/create-a-table-of-contents-for-ghost/
+// Function to initialize the table of contents for posts/pages
 function initializeToC() {
+  navHeight = document
+    .querySelector(".gh-navigation")
+    .getBoundingClientRect().height;
+
+  const header = document.querySelector(".gh-article-title");
+  postHeader = header.getBoundingClientRect().height;
+  const style = window.getComputedStyle(header);
+  const marginTop = parseInt(style.marginTop);
+  const marginBottom = parseInt(style.marginBottom);
+  postHeader += marginTop + marginBottom;
+
   const contentElement = document.querySelector(".gh-content");
   contentElement.id = "start";
   if (contentElement) {
     const asideElement = document.createElement("aside");
     asideElement.className = "gh-sidebar";
-    asideElement.innerHTML = '<div class="gh-toc" id="gh-toc"></div>';
+    asideElement.innerHTML = `
+    <div id="gh-mobile-toc">
+      <div class="toc-hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="gh-toc" id="gh-toc"></div>`;
     const firstChildElement = contentElement.firstElementChild;
     if (firstChildElement) {
       firstChildElement.insertAdjacentElement("beforebegin", asideElement);
@@ -30,7 +54,44 @@ function initializeToC() {
       scrollSmoothOffset: -100,
       headingsOffset: 100,
     });
+
+    // toggle the sidebar when the hamburger is clicked
+    document.getElementById("gh-mobile-toc").addEventListener("click", () => {
+      const sidebar = document.querySelector(".gh-sidebar");
+      sidebar.classList.toggle("fixed");
+    });
+
+    // hide the sidebar when a link is clicked
+    document.querySelectorAll(".toc-link").forEach((link) => {
+      link.addEventListener("click", () => {
+        const sidebar = document.querySelector(".gh-sidebar");
+        if (sidebar.classList.contains("fixed")) {
+          sidebar.classList.remove("fixed");
+        }
+      });
+    });
+
+    updateHamburgerDisplay();
   }
 }
 
+function updateHamburgerDisplay() {
+  const sidebar = document.querySelector(".gh-sidebar");
+  const sidebarBox = sidebar.getBoundingClientRect();
+  const mobileToc = document.getElementById("gh-mobile-toc");
+  const size = navHeight + postHeader + sidebarBox.height;
+
+  if (window.scrollY > size) {
+    mobileToc.classList.add("displayed");
+  } else {
+    mobileToc.classList.remove("displayed");
+
+    // Remove the fixed sidebar when the user scrolls back up
+    if (sidebar.classList.contains("fixed")) {
+      sidebar.classList.remove("fixed");
+    }
+  }
+}
+
+document.addEventListener("scroll", updateHamburgerDisplay);
 document.addEventListener("DOMContentLoaded", initializeToC);
