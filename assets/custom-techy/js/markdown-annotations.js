3f08131621bf7c533ec97ecd9aa1b6df774af68b
Fokklz <chat@fokklz.dev>
2024-11-16 12:15:35 +0100

    style: added a_utils.js to reuse some functions
    `a` to ensure its loaded before any other scripts.

--- assets/custom-techy/js/markdown-annotations.js
+++ assets/custom-techy/js/markdown-annotations.js
@@ -178,36 +178,5 @@ function findStartIndexToTrim(fullText) {
   }
 }
 
-// Function to build full text and node info
-function getFullTextAndNodeInfo(element) {
-  let fullText = "";
-  const nodeInfoList = [];
-
-  function traverse(node) {
-    if (node.nodeType === Node.TEXT_NODE) {
-      const textContent = node.textContent;
-      nodeInfoList.push({ node, length: textContent.length });
-      fullText += textContent;
-    } else if (node.nodeType === Node.ELEMENT_NODE) {
-      node.childNodes.forEach((child) => traverse(child));
-    }
-  }
-
-  traverse(element);
-  return { fullText, nodeInfoList };
-}
-
-// Function to map index to node and offset
-function getNodeAndOffsetFromIndex(nodeInfoList, index) {
-  let currentIndex = 0;
-  for (const info of nodeInfoList) {
-    if (currentIndex + info.length >= index) {
-      return { node: info.node, offset: index - currentIndex };
-    }
-    currentIndex += info.length;
-  }
-  return null;
-}
-
 // Call the main function after the DOM is loaded
 document.addEventListener("DOMContentLoaded", processCodeElements);
